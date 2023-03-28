bad503980457d9eaf6c273ead31e15e959fdb895
Polina Che <perhaybe@gmail.com>
2023-03-28 20:32:09 +0300

    fix: cors

--- utils/corsOptions.js
+++ utils/corsOptions.js
@@ -1,5 +1,4 @@
 const corsOption = {
-  // origin: ['http://localhost:3000', 'http://localhost:3005']
   origin: '*',
   optionsSuccessStatus: 200,
   methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
